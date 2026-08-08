e01e2a24c8f16454788610f63a8eb4ea9a4812aa
Drayken <27724118+Drayken@users.noreply.github.com>
2026-08-08 06:58:22 -0400

    Set to worker url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "http://localhost:4321",
+  site: "https://datashard.zxsn.workers.dev",
   trailingSlash: "ignore",
   prefetch: true,
   integrations: [mdx(), sitemap()],
