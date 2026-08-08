7a36431e93878547ba7993bd96651c78b627ae32
Drayken <27724118+Drayken@users.noreply.github.com>
2026-08-08 07:15:38 -0400

    period

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,2 +1,2 @@
 export const SITE_TITLE = "DATASHARD";
-export const SITE_DESCRIPTION = "Fragments and echoes from the digital frontier - an archive by Drayken";
+export const SITE_DESCRIPTION = "Fragments and echoes from the digital frontier - an archive by Drayken.";
