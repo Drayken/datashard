0b65903b10250ba3bb2aa2021cc0be0e0a2e5af7
Drayken <27724118+Drayken@users.noreply.github.com>
2026-08-09 07:45:15 -0400

    adjust desc

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,2 +1,2 @@
 export const SITE_TITLE = "ZXSN";
-export const SITE_DESCRIPTION = "Fragments and echoes from the digital frontier - an archive by Drayken.";
+export const SITE_DESCRIPTION = "Fragments of the Digital Frontier - Archive by Drayken";
